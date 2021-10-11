76ca727333d1029d7b0c6c56ef0a1c46d670d84d
Mohamed Achraf Karmous <achraf.karmous@gmail.com>
2021-10-11 04:52:44 +0200

    Loging System Finished

--- scripts/vue/Login_Form.js
+++ scripts/vue/Login_Form.js
@@ -16,28 +16,18 @@ var $login_form = $(`
     <br>
 </div>
 <div class="login-history">
-    <br>
-    <label class="Label" for="login">Enter your full name : </label>
-    <input type="text" id="fullName-input" name="fname">
-    <button id="save-fullName" type="button"> Save </button>
-    <br>
-    <br>
 </div>
 <div class="owned-product">
-    <br>
-    <label class="Label" for="login">Enter your full name : </label>
-    <input type="text" id="fullName-input" name="fname">
-    <button id="save-fullName" type="button"> Save </button>
-    <br>
-    <br>
 </div>`);
 $(".main-body").append($login_form);
 $(".user-fullName").hide();
+$(".login").hide();
 $("a").removeAttr("href");
 
 $("#login-btn").on("click", function () {
     // first we read the value of the tweet input
     var login = $("#login-input").val();
+    $("#login-input").val("");
     window.index = reduce(
         array_Users,
         function (index, element, i) {
@@ -49,7 +39,7 @@ $("#login-btn").on("click", function () {
         -1 // means not found
     );
     if (index >= 0) {
-        user = array_Users[index];
+        var user = array_Users[index];
 
         // create a new login_history
         var login_History = Login_History();
@@ -58,7 +48,7 @@ $("#login-btn").on("click", function () {
         login_History.set({ user_uid: user.uid });
         myDatabase.addElement("login_History", login_History);
         myDatabase.addElement("array_Users", array_Users);
-        myDatabase.addElement("uid", user.uid);
+        myDatabase.addElement("current_user", user);
         $(".login-container").hide();
         $(".login").hide();
         $(".disconnect").show();
@@ -71,28 +61,33 @@ $("#login-btn").on("click", function () {
             login: login,
             role_id: customer.id,
         });
+        myDatabase.addElement("current_user", user);
         $(".login-container").hide();
         $(".user-fullName").show();
     }
 });
 
 $("#save-fullName").on("click", function () {
-    console.log("AAAAAAAAAAAAAAAAAAAA");
-    console.log(index);
     // first we read the value of the tweet input
     var fullName = $("#fullName-input").val();
+    // Since it's a new user, we take the last user created & modify him
     var array_Users = myDatabase.object["array_Users"];
-    var user = array_Users[array_Users.length - 1];
-    user.set("full_Name", fullName);
+    array_Users[array_Users.length - 1].set({ full_Name: fullName });
 
     // create a new login_history
     var login_History = Login_History();
     array_Logins_History.new_Login_History_ID(login_History);
     array_Logins_History.add_Login_History(login_History);
-    login_History.set({ user_uid: user.uid });
+    login_History.set({ user_uid: array_Users[array_Users.length - 1].uid });
     myDatabase.addElement("login_History", login_History);
     myDatabase.addElement("array_Users", array_Users);
     $(".user-fullName").hide();
     $(".login").hide();
     $(".disconnect").show();
+});
+
+$(".disconnect").on("click", function () {
+    // first we read the value of the tweet input
+    $(".disconnect").hide();
+    $(".login-container").show();
 });
